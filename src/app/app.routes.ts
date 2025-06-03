7238cf4575e5eca5ce517e99658c71602071e3c1
Guilherme França <guilherme_capivari@hotmail.com>
2025-06-02 21:08:37 -0300

    refactor: Remove rotas e links desnecessários, substitui links de navegação por âncoras e ajusta conteúdo dos componentes de cabeçalho e rodapé para simplificar a navegação na landing page da GeoMAG.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
-import { AboutComponent } from './components/about/about.component';
-import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
-import { ServicesComponent } from './components/services/services.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'servicos', component: ServicesComponent },
-  { path: 'sobre', component: AboutComponent },
-  { path: 'contato', component: ContactComponent },
-  { path: '**', redirectTo: '' },
+  { path: '**', redirectTo: '' }
 ];
